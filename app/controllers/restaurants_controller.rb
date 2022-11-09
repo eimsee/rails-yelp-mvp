f694a21e11d28c6e33d929b3838cbe8bd83f48c7
marie christine <eimsee_eesmie@live.fr>
2022-11-09 09:55:30 -0400

    ok

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to_restaurant_path
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'creating restaurant...'
+le_vieux_foyal = { name: 'le vieux foyal', address: 'fort de france 97234', phone_number: '0696248754', category: "french" }
+hohio = { name: 'hohio', address: 'dillon 97264', phone_number: '0696876444', category: 'chinese' }
+poulpe = { name: 'poulpe', address: 'schoelcher 97234', phone_number: '0696842668', category: 'italian' }
+cabano = { name: 'cabano', address: 'robert 97234', phone_number: '0696574863', category: 'japanese' }
+douch = { name: 'douch', address: 'diamant 97234', phone_number: '0696647465', category: 'belgian' }
+[le_vieux_foyal, hohio, poulpe, cabano, douch].each do |restaurant|
+  restaurant = Restaurant.create!(restaurant)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
